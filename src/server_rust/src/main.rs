49209fba1df6043498f18c9f0a90219e016d1152
maiquel <jleahred@gmail.com>
2017-07-21 23:05:15 +0200

    testing server in rust

--- src/server_rust/src/main.rs
+++ src/server_rust/src/main.rs
@@ -1,48 +1,47 @@
 extern crate iron;
+extern crate staticfile;
+extern crate mount;
+extern crate ws;
 
 
 use iron::prelude::*;
-use iron::status;
+use std::path::Path;
 use mount::Mount;
+use staticfile::Static;
+use ws::listen;
+
+const SERVER_SOCKET: &str = "0.0.0.0:8080";
 
 fn main() {
-    let mut mount = Mount::new();
+    let mut static_path = Mount::new();
 
-    // Serve the shared JS/CSS at /
-    mount.mount("/", Static::new(Path::new("http_static")));
-    // Serve the static file docs at /doc/
-    // mount.mount("/doc/", Static::new(Path::new("target/doc/staticfile/")));
-    // // Serve the source code at /src/
-    // mount.mount("/src/",
-    //             Static::new(Path::new("target/doc/src/staticfile/lib.rs.html")));
-
-    Iron::new(mount).http("127.0.0.1:3000").unwrap();
+    static_path.mount("/", Static::new(Path::new("http_static")));
+    println!("server running on {}", SERVER_SOCKET);
+    Iron::new(static_path).http(SERVER_SOCKET).unwrap();
 }
 
-// fn main() {
-//     Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello World! aaaa"))))
-//         .http("0.0.0.0:3000")
-//         .unwrap();
-// }
-
-
-// extern crate iron;
-// extern crate staticfile;
-// extern crate mount;
-
-// use iron::prelude::*;
-// use iron::status;
 
 // fn main() {
-//     let mut mount = Mount::new();
 
-//     // Serve the shared JS/CSS at /
-//     mount.mount("/", Static::new(Path::new("http_static")));
-//     // Serve the static file docs at /doc/
-//     // mount.mount("/doc/", Static::new(Path::new("target/doc/staticfile/")));
-//     // // Serve the source code at /src/
-//     // mount.mount("/src/",
-//     //             Static::new(Path::new("target/doc/src/staticfile/lib.rs.html")));
+//     // Setup logging
+//     env_logger::init().unwrap();
 
-//     Iron::new(mount).http("127.0.0.1:3000").unwrap();
+//     // Listen on an address and call the closure for each connection
+//     if let Err(error) = listen("127.0.0.1:3012", |out| {
+
+//         // The handler needs to take ownership of out, so we use move
+//         move |msg| {
+
+//             // Handle messages received on this connection
+//             println!("Server got message '{}'. ", msg);
+
+//             // Use the out channel to send messages back
+//             out.send(msg)
+//         }
+
+//     }) {
+//         // Inform the user of failure
+//         println!("Failed to create WebSocket due to {:?}", error);
+//     }
+
 // }
